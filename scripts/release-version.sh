eb22b6dbc4e3d9b775c912830219399a6d8ddf1d
Trinh Minh Triet <contact@trinhminhtriet.com>
2025-01-02 11:13:39 +0700

    Update release script to automatically bump patch version and tag correctly

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -4,8 +4,10 @@ set -xe
 [ -z "$(git status --porcelain)" ] || (echo "dirty working directory" && exit 1)
 
 current_version="$(grep '^version = ' safeshell/Cargo.toml | head -1 | cut -d '"' -f2)"
-new_version="$1"
-new_version_num="${new_version#"v"}"
+IFS='.' read -r major minor patch <<<"$current_version"
+new_patch=$((patch + 1))
+new_version="$major.$minor.$new_patch"
+tag_name="v$new_version"
 
 if [ -z "$new_version" ]; then
     echo "New version required as argument"
@@ -13,7 +15,7 @@ if [ -z "$new_version" ]; then
 fi
 
 echo ">>> Bumping version"
-sed -i.bak "s/version = \"$current_version\"/version = \"$new_version_num\"/" safeshell/Cargo.toml
+sed -i.bak "s/version = \"$current_version\"/version = \"$new_version\"/" safeshell/Cargo.toml
 rm safeshell/Cargo.toml.bak
 
 sleep 5
@@ -21,8 +23,8 @@ sleep 5
 echo ">>> Commit"
 git add safeshell/Cargo.toml Cargo.lock
 git commit -am "version $new_version"
-git tag $new_version
+git tag $tag_name
 
 echo ">>> Publish"
 git push
-git push origin $new_version
+git push origin $tag_name
